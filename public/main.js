1681f4bf4b4a5cbc78d45a6fff8d27693944a401
GMorelle-2412 <geoffreymorelle2412@gmail.com>
2026-02-06 12:03:50 +0100

    connection_2

---
+++ public/main.js
@@ -0,0 +1,25 @@
+const connection = document.getElementById("button_connection");
+connection.addEventListener('click', () => {
+    const login = document.getElementById('login').value;
+    const password = document.getElementById('Password').value;
+
+    fetch('/connexion', {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({ login: login, password: password })
+    }).then(response => response.json())
+        .then(data => {
+            alert(data.message);
+            alert('ID utilisateur : ' + data.user.id);
+            localStorage.setItem("idUser", data.user.id);
+        });
+
+    fetch('/info')
+        .then(responsebrute => responsebrute.json())
+        .then(
+            responsejson => {
+                document.getElementById('reponse').innerHTML = responsejson.cle2;
+            });
+});
