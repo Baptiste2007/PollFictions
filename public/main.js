27cb2513399acaf61387ed45eb3e8e38cf77f91c
GMorelle-2412 <geoffreymorelle2412@gmail.com>
2026-02-12 14:14:53 +0100

    préparation oeuvres

--- public/main.js
+++ public/main.js
@@ -38,3 +38,41 @@ button_sign_up.addEventListener('click', () => {
             alert(data);
         });
 });
+
+const contenue_oeuvre = document.getElementById("contenue_oeuvre");
+contenue_oeuvre.addEventListener('click', () => {
+    
+    //voir servet.js
+    let idElecteur = localStorage.getItem("idUser");
+    //voir sur index.html
+    let idUser = document.getElementById("usersList").value;
+
+    //créer p
+    const newDiv = document.createElement("p");
+    //le contenue de p
+    const newContent = document.createTextNode("L'id Utilisateur " +
+        idElecteur + "  voter pour l'id " + idUser);
+    //créer un nouvelle div
+    const currentDiv = document.getElementById("div");
+
+    fetch('/Oeuvres', {
+
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({ idValue: idUser, idElecteur: idElecteur })
+    }).then(response => response.text())
+        .then(data => {
+            alert("L'id Utilisateur " + idElecteur + "  voter pour l'id " + idUser);
+        });
+        //asemblement de p et du contenu de p
+        newDiv.appendChild(newContent);
+        //asemblement de la div avec l'asemblement de p et du contenu de p
+        document.body.insertBefore(newDiv, currentDiv);
+    
+
+});
+
+
+
