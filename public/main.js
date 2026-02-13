0b5c2ac7025eb21c6fb902145a354baf8f7abf16
GMorelle-2412 <geoffreymorelle2412@gmail.com>
2026-02-13 10:46:22 +0100

    modif perso

--- public/main.js
+++ public/main.js
@@ -41,39 +41,20 @@ button_sign_up.addEventListener('click', () => {
 
 const contenue_oeuvre = document.getElementById("contenue_oeuvre");
 contenue_oeuvre.addEventListener('click', () => {
-    
-    //voir servet.js
-    let idElecteur = localStorage.getItem("idUser");
-    //voir sur index.html
-    let idUser = document.getElementById("usersList").value;
-
-    //créer p
-    
-    const newDiv = document.createElement("p");
-    //le contenue de p
-    const newContent = document.createTextNode("L'id Utilisateur " +
-        idElecteur + "  voter pour l'id " + idUser);
-    //créer un nouvelle div
-    const currentDiv = document.getElementById("div");
 
     fetch('/Oeuvres', {
-
-        method: 'POST',
+        method: 'GET',
         headers: {
             'Content-Type': 'application/json'
         },
-        body: JSON.stringify({ idValue: idUser, idElecteur: idElecteur })
-    }).then(response => response.text())
-        .then(data => {
-            alert("L'id Utilisateur " + idElecteur + "  voter pour l'id " + idUser);
-        });
-        //asemblement de p et du contenu de p
-        newDiv.appendChild(newContent);
-        //asemblement de la div avec l'asemblement de p et du contenu de p
-        document.body.insertBefore(newDiv, currentDiv);
-    
+    })
+    .then(response => response.json())   // <-- JSON obligatoire
+    .then(data => {
+        const div = document.getElementById("gen_contenue_oeuvre");
+        const h2 = document.createElement("h2");
 
+        h2.textContent = data[0].Titre;  // <-- Premier titre du tableau
+        div.appendChild(h2);
+    })
+    .catch(err => console.error(err));
 });
-
-
-
